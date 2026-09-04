fb575f0cfcef6bff35ea8f0c15320bcb3d0836d3
fellimohamed22-source <fellimohamed22@gmail.com>
2026-09-05 01:47:15 +0200

    feat: wire Angular operations

--- web/src/routes.ts
+++ web/src/routes.ts
@@ -1,1 +1,15 @@
-import {Routes} from '@angular/router';import {Partner} from './views/partner';import {Admin} from './views/admin';import {Finance} from './views/finance';import {Support} from './views/support';export const routes:Routes=[{path:'partner',component:Partner},{path:'admin',component:Admin},{path:'finance',component:Finance},{path:'support',component:Support},{path:'',pathMatch:'full',redirectTo:'partner'}];
+import {Routes} from '@angular/router';
+import {Login} from './views/login';
+import {Partner} from './views/partner';
+import {Admin} from './views/admin';
+import {Finance} from './views/finance';
+import {Support} from './views/support';
+
+export const routes:Routes=[
+  {path:'login',component:Login},
+  {path:'partner',component:Partner},
+  {path:'admin',component:Admin},
+  {path:'finance',component:Finance},
+  {path:'support',component:Support},
+  {path:'',pathMatch:'full',redirectTo:'login'}
+];
