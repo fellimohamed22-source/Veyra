3b130a98dde486338f810bfe9242615ad4ad3a67
fellimohamed22-source <fellimohamed22@gmail.com>
2026-09-11 15:02:02 +0200

    chore(client-maps): remove obsolete direct flutter_map import

--- mobile-client/lib/main.dart
+++ mobile-client/lib/main.dart
@@ -7,7 +7,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter_stripe/flutter_stripe.dart' hide Card;
-import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'api.dart';
