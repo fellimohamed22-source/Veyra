320e2730228518f3e8a482959eb2cc5bd62c8439
fellimohamed22-source <fellimohamed22@gmail.com>
2026-09-20 01:33:17 +0200

    fix(driver): restore profile imports for analyzer

--- mobile-driver/lib/main.dart
+++ mobile-driver/lib/main.dart
@@ -1,11 +1,12 @@
 import 'dart:async';
+import 'dart:typed_data';
 import 'package:dio/dio.dart';
 
 import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter/material.dart';
-import 'package:file_picker/file_picker.dart' as picker show FilePicker, FileType;
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
