4f67cd68fc1a370f35f72766961803975af4bd2e
fellimohamed22-source <fellimohamed22@gmail.com>
2026-09-19 14:06:54 +0200

    fix(driver): expose file type through picker bridge

--- mobile-driver/lib/file_picker_bridge.dart
+++ mobile-driver/lib/file_picker_bridge.dart
@@ -1,1 +1,1 @@
-export 'package:file_picker/file_picker.dart' show FilePicker, FilePickerResult, PlatformFile;
+export 'package:file_picker/file_picker.dart' show FilePicker, FilePickerResult, PlatformFile, FileType;
