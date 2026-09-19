8427ef7fbfc9f4c4ccd0d5f387825a5a1cab1382
fellimohamed22-source <fellimohamed22@gmail.com>
2026-09-19 10:31:55 +0200

    fix(driver): avoid file picker Flutter widget name collisions

--- mobile-driver/lib/main.dart
+++ mobile-driver/lib/main.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'package:dio/dio.dart';
-import 'package:file_picker/file_picker.dart';
+import 'package:file_picker/file_picker.dart' hide Padding, SizedBox, FutureBuilder;
 import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:url_launcher/url_launcher.dart';
